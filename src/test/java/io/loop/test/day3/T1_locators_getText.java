1cd6b948d922370e07a37e9e17230a766a57a0f1
andre <lana6391@gmail.com>
2024-06-09 14:59:19 -0400

    Changes for day_3, locators and (getText and other)

---
+++ src/test/java/io/loop/test/day3/T1_locators_getText.java
@@ -0,0 +1,45 @@
+package io.loop.test.day3;
+
+import io.loop.test.utilities.DocuportConstants;
+import io.loop.test.utilities.WebDriverUtil;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_locators_getText {
+    /*
+     * go to docuport
+     * enter username
+     * do NOT enter password
+     * click on login button
+     * verify error message - please enter password
+     */
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverUtil.getDriver("chrome");
+        driver.manage().window().maximize();
+
+        // go to the url
+        driver.get("https://beta.docuport.app/login");
+
+        WebElement userName = driver.findElement(By.id("input-14"));
+        userName.sendKeys(DocuportConstants.USERNAME_CLIENT);
+
+        WebElement loginButton = driver.findElement(By.className("v-btn__content"));
+        loginButton.click();
+
+        WebElement errorMessage = driver.findElement(By.className("v-messages__message"));
+        String actualErrorMessage = errorMessage.getText();
+
+        if (actualErrorMessage.equals(DocuportConstants.ERROR_MESSAGE_FOR_EMPTY_PASSWORD)){
+            System.out.println("Expected error message: " + DocuportConstants.ERROR_MESSAGE_FOR_EMPTY_PASSWORD + ", matches actual error message: " + actualErrorMessage + " => TEST PASS");
+        } else {
+            System.err.println("Expected error message: " + DocuportConstants.ERROR_MESSAGE_FOR_EMPTY_PASSWORD + ", DOES NOT MATCH actual error message: " + actualErrorMessage + " => TEST FAIL");
+        }
+
+        driver.quit();
+
+
+    }
+}
