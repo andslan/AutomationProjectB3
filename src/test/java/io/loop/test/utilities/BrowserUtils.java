4422ed3b4a0a29610177bd0caea1ac360497c2d8
andre <lana6391@gmail.com>
2024-07-19 21:38:42 -0400

    Changes for day_10

--- src/test/java/io/loop/test/utilities/BrowserUtils.java
+++ src/test/java/io/loop/test/utilities/BrowserUtils.java
@@ -1,7 +1,14 @@
 package io.loop.test.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Set;
 
 import static org.testng.Assert.assertTrue;
@@ -10,6 +17,7 @@ public class BrowserUtils {
 
     /**
      * validate if driver switched to expected url and title
+     *
      * @param driver
      * @param expectedUrl
      * @param expectedTitle
@@ -35,7 +43,6 @@ public class BrowserUtils {
     }
 
     /**
-     *
      * @param driver
      * @param targetTitle
      * @author nadir
@@ -44,10 +51,104 @@ public class BrowserUtils {
         String origin = driver.getWindowHandle();
         for (String handle : driver.getWindowHandles()) {
             driver.switchTo().window(handle);
-            if(driver.getTitle().contains(targetTitle)) {
+            if (driver.getTitle().contains(targetTitle)) {
                 return;
             }
         }
         driver.switchTo().window(origin);
     }
+
+    /**
+     * @param driver
+     * @param expectedTitle returns void, assertion is implemented
+     * @author nadir
+     */
+    public static void validateTitle(WebDriver driver, String expectedTitle) {
+        assertTrue(driver.getTitle().contains(expectedTitle));
+    }
+
+    /**
+     * Click any link from loop practice
+     * @param nameOfPage
+     * @author nadir
+     */
+    public static void loopLinkClick(String nameOfPage){
+        WebElement element = Driver.getDriver().findElement(By.xpath("//a[.='"+nameOfPage+"']"));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+    }
+
+    /**
+     * Moves the mouse to given element
+     * @param element on which to hover
+     * @author nadir
+     */
+    public static void hover(WebElement element){
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(element).perform();
+    }
+
+    /**
+     * Scrolls down to an element using JavaScript
+     * @param element
+     * @author nadir
+     */
+    public static void scrollToElement(WebElement element){
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    /**
+     * Clicks on an element using JavaScript
+     * @param element
+     * @author nadir
+     */
+    public static void clickWithJS(WebElement element){
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();", element);
+    }
+
+    /**
+     * Performs double click action on an element
+     * @param element
+     * @author nadir
+     */
+    public static void doubleClick(WebElement element){
+        new Actions(Driver.getDriver()).doubleClick(element).perform();
+    }
+
+    /**
+     * Waits for the provided element to be visible on the page
+     * @param element
+     * @param timeToWaitInSec
+     * @return
+     * @author nadir
+     */
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeToWaitInSec));
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    /**
+     * Waits for provided element to be clickable
+     * @param element
+     * @param timeout
+     * @return
+     * @author nadir
+     */
+    public static WebElement waitForClickable (WebElement element, int timeout){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    /**
+     * Waits for the provided element to be invisible on the page
+     * @param element
+     * @param timeToWaitInSec
+     * @author nadir
+     */
+    public static void waitForInVisibility(WebElement element, int timeToWaitInSec){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeToWaitInSec));
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
 }
